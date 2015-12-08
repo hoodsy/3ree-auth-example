9e3ea5ca511b9fd89bc1bda736b94be0edb889cb
Logan Bernard <bernard.logan4@gmail.com>
2015-12-08 16:21:45 -0500

    remove returns in actionCreators

--- app/state/actions/actions.js
+++ app/state/actions/actions.js
@@ -3,8 +3,8 @@ import * as types from '../constants/actionTypes'
 // Todos
 // =====
 export const addTodo = (text, listIndex) => { type: types.ADD_TODO, text, listIndex }
-export const completeTodo = (listIndex, index) => return { type: types.COMPLETE_TODO, listIndex, index }
+export const completeTodo = (listIndex, index) => { type: types.COMPLETE_TODO, listIndex, index }
 
 // Lists
 // =====
-export const addList = (text) => return { type: types.ADD_LIST, text }
+export const addList = (text) => { type: types.ADD_LIST, text }
