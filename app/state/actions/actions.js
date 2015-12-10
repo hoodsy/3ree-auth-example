7035625b8c37118bfffcdfe3a0be49913cf6b5e0
Dylan Bannon <drb2857@rit.edu>
2015-12-09 21:47:20 -0500

    fixed syntax in actions.js

--- app/state/actions/actions.js
+++ app/state/actions/actions.js
@@ -2,9 +2,9 @@ import * as types from '../constants/actionTypes'
 
 // Todos
 // =====
-export const addTodo = (text, listIndex) => { type: types.ADD_TODO, text, listIndex }
-export const completeTodo = (listIndex, index) => { type: types.COMPLETE_TODO, listIndex, index }
+export const addTodo = (text, listIndex) => ({ type: types.ADD_TODO, text, listIndex })
+export const completeTodo = (listIndex, index) => ({ type: types.COMPLETE_TODO, listIndex, index })
 
 // Lists
 // =====
-export const addList = (text) => { type: types.ADD_LIST, text }
+export const addList = (text) => ({ type: types.ADD_LIST, text })
