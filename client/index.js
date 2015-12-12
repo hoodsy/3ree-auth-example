2f96740074c026acf7e72ec8f47762a1efcb93de
Logan Bernard <bernard.logan4@gmail.com>
2015-12-12 09:27:46 -0500

    fixed server->client connection

--- client/index.js
+++ client/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+
 import { Provider } from 'react-redux';
+import { DevTools,
+         DebugPanel,
+         LogMonitor } from 'redux-devtools/lib/react';
+import { createStore } from 'redux';
+
 import App from '../common/views/containers/App';
 import todoApp from '../common/state/reducers/reducers';
 import configureStore from '../common/state/stores/configureStore';
-import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
 const initialState = window.__INITIAL_STATE__;
 const store = configureStore(todoApp, initialState);
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ var config = {
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"development"' })
   ],
   module: {
     loaders: [{
