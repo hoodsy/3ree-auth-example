10c18a96607150db274c52c2f12390601f3b169f
Logan Bernard <bernard.logan4@gmail.com>
2016-02-07 09:58:13 -0500

    [webpack] hmr - without stateless components

--- common/state/stores/configureStore.js
+++ common/state/stores/configureStore.js
@@ -61,13 +61,7 @@ if (typeof window !== 'undefined') {
 // ====================
 export default function configureStore(initialState) {
   const store = createStoreWithMiddleware(rootReducer, initialState)
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      const nextReducer = require('../reducers')
-      store.replaceReducer(nextReducer)
-    })
-  }
+  if (module.hot) module.hot.accept()
   return store
 }
 
