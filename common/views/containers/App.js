c636a36805248d4f76a7c6defac00ced69fca094
Logan Bernard <bernard.logan4@gmail.com>
2016-02-08 20:22:16 -0500

    add global padding

--- common/views/containers/App.js
+++ common/views/containers/App.js
@@ -3,10 +3,14 @@ import React, { PropTypes } from 'react'
 export const App = (
   { children }) =>
 (
-  <div>
+  <div style={ styles }>
     { children }
   </div>
 )
+
+const styles = {
+  padding: '5% 10%'
+}
 
 App.propTypes = {
   children: PropTypes.object
